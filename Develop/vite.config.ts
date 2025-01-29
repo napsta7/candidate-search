7c562af6bb5b75fa6db9c12cf5551a8bdc60dee4
Paige <paige.renee395@gmail.com>
2025-01-28 18:09:49 -0600

    Trying to fix port issues with render

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 3000, // Use the PORT environment variable or default to 3000
+  },
 });
