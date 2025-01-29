4e77be72e8b5e91e428645a57ce97b20889b3d87
Paige <paige.renee395@gmail.com>
2025-01-28 19:06:07 -0600

    Still trying to fix deploy

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
-  base: '/',
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 3000,
     host: true,
