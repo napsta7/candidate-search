db4b3743fcdda54db8c43d3fa26a4876c8ac48d1
Paige <paige.renee395@gmail.com>
2025-01-28 19:03:41 -0600

    Trying to fix render issues

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  base: '/',
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 3000,
     host: true,
