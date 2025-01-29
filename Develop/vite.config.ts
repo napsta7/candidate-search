982721be0ef511d05e4151e2c381628fde5080b6
Paige <paige.renee395@gmail.com>
2025-01-28 18:42:02 -0600

    still trying to fix port issues

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   envDir: './environment',
   plugins: [react()],
   server: {
-    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
   },
 });
